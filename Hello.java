b667dcc0154885f14211e7a3555eaa40b9363572
jintaejeon <jjt8989@gmail.com>
2016-07-04 15:45:57 +0900

    added output "That's DVCS"

--- Hello.java
+++ Hello.java
@@ -1,11 +1,8 @@
 public class Hello{
   public static void main(String[] args){
     System.out.println("Hello Git");
-<<<<<<< HEAD
-    System.out.println("TESTTTT");
- } 
-=======
 	System.out.println("sisTT");
+	
+
 }
->>>>>>> sist
 }
