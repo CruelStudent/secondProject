8e51f700bf25373e99522b434166eef03a60df9b
CruelStudent <jjt8989@gmail.com>
2016-07-20 15:16:33 +0900

    Hello.java update

--- Hello.java
+++ Hello.java
@@ -1,9 +1,10 @@
+//for command line git tutorial
+//GitHub odification 
 public class Hello{
   public static void main(String[] args){
     System.out.println("Hello Git");
 	System.out.println("sisTT");
 <<<<<<< HEAD
-	
 
 }
 =======
